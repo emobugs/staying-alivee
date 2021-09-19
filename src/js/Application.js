1fd7c5d54087a3a73ffadcfacd0ec7a86c2904f9
Emo <emotupev11@gmail.com>
2021-09-19 13:29:37 +0300

    feat: all need changes

--- src/js/Application.js
+++ src/js/Application.js
@@ -14,12 +14,26 @@ export default class Application extends EventEmitter {
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
+    this._beat = new Beat();
 
-    document.querySelector(".main").appendChild(message);
+		this._beat.addListener(Beat.events.BIT, () => {
+			this._create(lyrics[count]);
+			count = Math.abs(++count % lyrics.length);
+		});
+
+    // const message = document.createElement("div");
+    // message.classList.add("message");
+    // message.innerText = "Ah";
+
+    // document.querySelector(".main").appendChild(message);
 
     this.emit(Application.events.READY);
   }
+
+  _create(text) {
+		const message = document.createElement('div');
+		message.classList.add('message');
+		message.innerText = text;
+		document.querySelector('.main').appendChild(message);
+	}
 }
